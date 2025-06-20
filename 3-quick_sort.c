9c1d3cfe882daa2beb2e376b10910851de813272
Abdullah <assf23134@icloud.com>
2025-06-20 18:30:15 +0000

    Edit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -34,6 +34,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 		int pi = partition(array, low, high, size);
+
 		quick_sort_recursive(array, low, pi - 1, size);
 		quick_sort_recursive(array, pi + 1, high, size);
 	}
